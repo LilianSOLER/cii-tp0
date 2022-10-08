80703128b9721d12d5c779c064e84fc36bc55093
bracquib <benjaminbracquier@gmail.com>
2022-10-08 22:44:54 +0200

    tracé aproximatif avec repère

--- trace.py
+++ trace.py
@@ -4,19 +4,41 @@
 import sys
 from math import *
 
-
 def trace(function, xmin, xmax, nstep, output):
-    output.write("x, %s\n" % function)
+    print(output.name)
+    tpm = output.name.split(".")
+    print(tpm)
+    if(tpm[1] != "ps"):
+        output.write("x, %s\n" % function)
+    else :
+        output.write("%!\nnewpath\n100 600 moveto\n(" + str(xmin) + ") show\n0 -400 rlineto\n(" + str(xmax) + ") show\nclosepath\n400 0 rlineto\nclosepath\n")
+
     function = eval("lambda x:" + function)
 
-    step = 1. * (xmax - xmin) / nstep
+
+    oldy = 0
+
+    step = (xmax - xmin) / nstep
     for i in range(nstep + 1):
         x = xmin + i * step
         try:
             y = function(x)
         except:
             continue
-        output.write("%s, %s\n" % (x, y))
+
+        if(tpm[1] != "ps"):
+            output.write("x, %s\n" % function)
+        else :
+            output.write("%s %s rlineto\n" % (200/nstep, -(oldy-y)*200))
+            #output.write("%s %s lineto\n" % ((200)+x*(200/xmax), 200+y*200))
+            #output.write("%s %s lineto\n" % ((200)+x*(200/xmax), 200+y*200))
+            oldy = y   
+        
+        #output.write("%s, %s\n" % (x, y))
+
+    output.write("stroke\n")
+    output.write("showpage\n")
+
 
 
 def usage():
@@ -28,6 +50,7 @@ def usage():
         -o, --output: output file
         -x, --xmin: minimum value of x
         -X, --xmax: maximum value of x
+        -n, --nstep: number of step
         """
     sys.stderr.write(dedent(txt))
 
@@ -38,7 +61,7 @@ def main(argv=None):
 
     import getopt
     try:
-        options, argv = getopt.getopt(argv[1:], "ho:x:X:", ["help", "output=", "xmin=", "xmax="])
+        options, argv = getopt.getopt(argv[1:], "ho:x:X:n:", ["help", "output=", "xmin=", "xmax=", "nstep="])
     except getopt.GetoptError as message:
         sys.stderr.write("%s\n" % message)
         sys.stderr.write("use -h for help\n")
@@ -50,8 +73,6 @@ def main(argv=None):
     nstep = 10
 
     for option, value in options:
-        print("option: " + option)
-        print("value: " + value)
         if option in ("-h", "--help"):
             usage()
             sys.exit(0)
@@ -61,6 +82,8 @@ def main(argv=None):
             xmin = float(value)
         elif option in ["-X", "--xmax"]:
             xmax = float(value)
+        elif option in ["-n", "--nstep"]:
+            nstep = int(value)
         else:
             assert False, "unhandled option"
 
